715d42b060a6fb14e31da7e7e1a0da356101a6cc
hlgirard <hl.girard@gmail.com>
2018-11-24 17:27:27 -0500

    Doesn't activate the servo if the change in temperature is < 1C

--- weatherclock.py
+++ weatherclock.py
@@ -9,7 +9,7 @@ import hardwareControl
 TEMP_SERVO_PIN = 14
 MIN_TEMP = -25
 MAX_TEMP = 50
-LOOP_DELAY = 10 #seconds
+LOOP_DELAY = 60 * 10 # 10 minutes = 600 seconds
 
 
 def print_weather(jWeatherData):
@@ -24,12 +24,15 @@ def main():
     servo = hardwareControl.TempServo(TEMP_SERVO_PIN, MIN_TEMP, MAX_TEMP)
 
     #Main Loop
+    prevTemp = 0
     while True:
         w = datarepo.get_weather()
         temp_c = w['main']['temp'] - 273.15 #K to C conversion
         print('The current temperature is {} ºC'.format(temp_c))
-        servo.setTemp(temp_c)
-
+        if abs(prevTemp - temp_c) > 1:
+                servo.setTemp(temp_c)
+                prevTemp = temp_c
+        
         sleep(LOOP_DELAY)
 
 
